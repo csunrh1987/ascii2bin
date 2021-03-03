b216d63ab588e3082f1607be6dd7d37e2509e4f2
csunrh1987 <randy.herrera.928@my.csun.edu>
2021-03-02 16:17:26 -0800

    v1

---
+++ ascii2bin.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main (int argc, char * argv[], char ** envp) {
+	
+	
+	
+	
+	int retval;
+	char ascii_value=0;
+	char digit=0;
+	char offset = 0x30;
+	int number=0;
+	
+    retval = read(0, &ascii_value, 1);
+    while (retval == 1){
+		
+		if(ascii_value == 0x30 || ascii_value == 0x31) {
+			digit = ascii_value - offset;
+			number = (number << 1) + digit;  
+			retval = read(0, &ascii_value, 1);
+			}
+		else {
+			retval = 0;
+			}
+	}
+
+    printf("%u\n", number);
+	return 0;
+	}
